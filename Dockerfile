53793b9cdea3e63805c097de7242552630fdaaab
Brian Malinconico <bmalinconico@terminus.com>
2018-12-11 15:30:55 -0500

    testing

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ruby:2.5-alpine
 
+RUN apk --no-cache add git bash
+
 RUN mkdir -p /app
 WORKDIR /app
 COPY Gemfile Gemfile.lock /app/
--- generate-service-overview.rb
+++ generate-service-overview.rb
@@ -14,15 +14,24 @@ OptionParser.new do |opts|
   opts.on('-a', '--convox-app-name APP_NAME', 'Convox app name') do |v|
     options[:app_name] = v
   end
+
+	opts.on("-f", "--convox-file CONVOX_FILE_NAME", "Convox file to parse for information (default: docker-compose.convox.yml)") do |v|
+		options[:file] = v
+	end
+
+	opts.on("-o", "--output-file TERRAFORM_TARGET_FILE", "target terraform file") do |v|
+		options[:output_file] = v
+	end
 end.parse!
 
 raise ArgumentError, 'Missing convox app name  (define with -a)' unless options[:app_name]
+raise ArgumentError, "Missing target Terraform file for output  (define with -o)" unless options[:output_file]
 
 repo = options[:repo]
 convox_app = options[:app_name]
 
-config = YAML.safe_load(File.read('/tmp/input-file.tmp'))
+config = YAML.safe_load(File.read(options[:file]))
 
 template = ERB.new(File.read(File.join(__dir__, 'templates', 'services-overview.erb')))
 
-File.write('/tmp/generated-output.tmp', template.result(binding))
+File.write(options[:output_file], template.result(binding))
